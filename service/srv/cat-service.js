b6989bce09f3d1bfe9a9f387d208a6ca9ae1edf3
Tabakov <bel2017@gmail.com>
2023-02-20 00:18:39 +0300

    leftJoin fix

--- service/srv/cat-service.js
+++ service/srv/cat-service.js
@@ -13,24 +13,18 @@ module.exports = class CatalogService extends cds.ApplicationService {
             return await service.tx(req).run(req.query); 
         }
         const Topics_ID = req?.query?.SELECT?.where[2].val;
-        let query = SELECT.from(`${Customers.name} as C`)
+        const query = SELECT.from(`${Customers.name} as C`)
                 .columns ('C.name', 'C.ID', 'C.customerId', 'M.manualErpId as erpId2', 'C.erpId')
-                .join(`${ManualAddedErpIDs.name} as M`)
+                .leftJoin(`${ManualAddedErpIDs.name} as M`)
                 .on('C.customerId = M.customerId').and('C.erpId = M.erpId')
                 .where({'C.Topics_ID':Topics_ID})
-                // case
-                // .columns ('F.status as fbaStatus').join(`${Fba.name} as F`)
-                // .on('C.erpId = F.erpId')
-        query.SELECT.from.join = 'left';
-        
-         let result = await service.tx(req).run(query);
-         result = result.map((elem) => {
+        let result = await service.tx(req).run(query);
+        result = result.map((elem) => {
             if (elem.erpId2) {
                 elem.erpId = elem.erpId2;
             }
-
-            return elem
-         })
-         return result;
+            return elem;
+        })
+        return result;
     }
 }
